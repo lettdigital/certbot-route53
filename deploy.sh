f450be9b37d927399aeb85cacbb8ba3a0e25df1c
Gabriel Chamon Araujo <gchamon@live.com>
2020-06-25 11:33:09 -0300

    certbot email and target bucket now deploy.sh arguments

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,24 @@
 set -e
 
 DOMAIN_NAME=$1
+CERTBOT_EMAIL=$2
+TARGET_BUCKET=$3
 
-if [ -z "$DOMAIN_NAME" ]; then
-  echo "Usage: ./deploy.sh <domain_name>"
+if [ -z "$DOMAIN_NAME" ] || [ -z "$CERTBOT_EMAIL" ] || [ -z "$TARGET_BUCKET" ]; then
+  echo "Usage: ./deploy.sh <domain_name> <certbot_email> <target_bucket>"
   exit 1
 fi
 
-(aws s3 cp s3://lett-ssl-certificates/backups/"$DOMAIN_NAME".tar.gz . && tar -xzvf "$DOMAIN_NAME".tar.gz .) || true
+(aws s3 cp s3://"$TARGET_BUCKET"/backups/"$DOMAIN_NAME".tar.gz . && tar -xzvf "$DOMAIN_NAME".tar.gz .) || true
 
-sed "s/{{\s*domain_name\s*}}/$DOMAIN_NAME/g" docker-compose.yml.j2 > docker-compose.yml
+sed "s/{{\s*domain_name\s*}}/$DOMAIN_NAME/g" docker-compose.yml.j2 |
+  sed "s/{{\s*email\s*}}/$CERTBOT_EMAIL/g" > docker-compose.yml
+
 docker-compose pull
 docker-compose run --rm certbot
 sudo chown -R "$(id -un)": certbot
 
 tar -czvf "$DOMAIN_NAME".tar.gz ./certbot
-aws s3 cp "$DOMAIN_NAME".tar.gz s3://lett-ssl-certificates/backups/"$DOMAIN_NAME".tar.gz
+aws s3 cp "$DOMAIN_NAME".tar.gz s3://"$TARGET_BUCKET"/backups/"$DOMAIN_NAME".tar.gz
 
-aws s3 sync --exclude '*README*' ./certbot/conf/live/"$DOMAIN_NAME" s3://lett-ssl-certificates/"$DOMAIN_NAME"
+aws s3 sync --exclude '*README*' ./certbot/conf/live/"$DOMAIN_NAME" s3://"$TARGET_BUCKET"/"$DOMAIN_NAME"
